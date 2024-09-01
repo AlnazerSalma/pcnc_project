76a76637b56402c6de924c0e168f245d1022d4d9
AlnazerSalma <202007690@bethlehem.edu>
2024-09-01 21:47:20 +0300

    remove the type from font size

--- lib/util/font_sizes.dart
+++ lib/util/font_sizes.dart
@@ -1,6 +1,6 @@
-const double textTiny = 10.0;
-const double textSmall = 12.0;
-const double textMedium = 14.0;
-const double textExtraLarge = 18.0;
-const double textXExtraLarge = 20.0;
-const double textBold = 30.0;
+const textTiny = 10.0;
+const textSmall = 12.0;
+const textMedium = 14.0;
+const textExtraLarge = 18.0;
+const textXExtraLarge = 20.0;
+const textBold = 30.0;
